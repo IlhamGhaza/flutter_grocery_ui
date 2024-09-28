137c55fa2ff3032db1e4ffe44cafa9815aa5c00b
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-09-28 12:25:44 +0700

    feat: :tada: init

---
+++ lib/presentation/onboard_pages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnboardPages extends StatelessWidget {
+  const OnboardPages({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
